da5619bcdf4f2e97be9502ae55db5494f1464af1
Jeffrey Druin <jdruin1@gmail.com>
2018-02-12 19:59:09 -0600

    Adding exceptions

---
+++ Druin.Chef.Core/Exceptions/ChefException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Druin.Chef.Core.Exceptions
+{
+    public class ChefException : Exception
+    {
+        public ChefException()
+        {}
+
+        public ChefException(string message):base(message)
+        {}
+
+        public ChefException(string message, Exception inner): base(message, inner)
+        { }
+    }
+}
