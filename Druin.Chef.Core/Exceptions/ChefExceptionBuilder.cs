6bc2e08f8e581c781ae7574fbf1531f91b7b7e4b
Jeffrey Druin <jdruin1@gmail.com>
2018-02-15 06:33:56 -0600

    Moar exceptions work

--- Druin.Chef.Core/Exceptions/ChefExceptionBuilder.cs
+++ Druin.Chef.Core/Exceptions/ChefExceptionBuilder.cs
@@ -3,14 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.Net;
 
 namespace Druin.Chef.Core.Exceptions
 {
-    public class ChefExceptionBuilder
+    public class ChefExceptionBuilder : Exception
     {
-        public static void Build(string username, Exception exception)
+        public ChefExceptionBuilder(string username, WebException exception)
         {
-
+            Build(username, exception);
+        }
+        private void Build(string username, WebException exception)
+        {
+            HttpWebResponse res = (HttpWebResponse)exception.Response;
+            string message;
+            switch (res.StatusCode)
+            {
+                case HttpStatusCode.BadRequest:
+                    message = "Bad request. The contents of the request are not formatted correctly.";
+                    throw new ChefException(message);
+                case HttpStatusCode.Unauthorized:
+                    message = "Unauthorized. The user or client (" + username + ") could not be authenticated. Verify the user/client name, and that the correct key was used to sign the request.";
+                    throw new ChefUnauthorizedException(message);
+                case HttpStatusCode.Forbidden:
+                    message = "Forbidden. " + username + " is not authorized to perform the action.";
+                    throw new ChefForbiddenException(message);
+                case HttpStatusCode.NotFound:
+                    message = "Not found. The requested object does not exist.";
+                    throw new ChefNotFoundException(message);
+                case HttpStatusCode.Conflict:
+                    message = "Conflict. The object already exists.";
+                    throw new ChefConflictException(message);
+                case HttpStatusCode.Gone:
+                    message = "The object is gone";
+                    throw new ChefGoneException(message);
+                case HttpStatusCode.RequestEntityTooLarge:
+                    message = "Request entity too large. A request may not be larger than 1000000 bytes.";
+                    throw new ChefTooLargeException(message);
+                case HttpStatusCode.InternalServerError:
+                    message = "Internal Server Error";
+                    throw new ChefException(message, exception);
+                default:
+                    message = res.StatusDescription;
+                    throw new ChefException(message, exception);
+            }
         }
     }
 }
